93b89dfd0e7295a4d0f24d945a9dc6d6655378e7
Jatin Sharma <jatts0010@gmail.com>
2026-02-13 04:06:38 +0530

    animation loop uses

--- src/main.ts
+++ src/main.ts
@@ -240,35 +240,83 @@
 
 /* RENDERER LEARNING */
 
+// import './style.css';
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+// const scene = new THREE.Scene();
+
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   // window.innerWidth / window.innerHeight,
+//   2,
+//   0.1,
+//   1000,
+// );
+// camera.position.z = 1.5;
+
+// const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+// const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+// renderer.setSize(400, 200);
+// // const renderer = new THREE.WebGLRenderer();
+// // renderer.setSize(window.innerWidth, window.innerHeight);
+// // document.body.appendChild(renderer.domElement);
+
+// // window.addEventListener('resize', () => {
+// //   camera.aspect = window.innerWidth / window.innerHeight;
+// //   camera.updateProjectionMatrix();
+// //   renderer.setSize(window.innerWidth, window.innerHeight);
+// // });
+
+// new OrbitControls(camera, renderer.domElement);
+
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshNormalMaterial({ wireframe: true });
+
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   renderer.render(scene, camera);
+// }
+
+// animate();
+
+/* ANIMATION LOOP */
+
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import Stats from 'three/addons/libs/stats.module.js';
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
   75,
-  // window.innerWidth / window.innerHeight,
-  2,
+  window.innerWidth / window.innerHeight,
   0.1,
   1000,
 );
 camera.position.z = 1.5;
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
-renderer.setSize(400, 200);
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// window.addEventListener('resize', () => {
-//   camera.aspect = window.innerWidth / window.innerHeight;
-//   camera.updateProjectionMatrix();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-// });
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera); // on-demand re-rendering
+});
 
-new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', function () {
+  // on demand re-rendering (rendered once initially)
+  renderer.render(scene, camera);
+});
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshNormalMaterial({ wireframe: true });
@@ -276,10 +324,25 @@ const material = new THREE.MeshNormalMaterial({ wireframe: true });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-function animate() {
-  requestAnimationFrame(animate);
+const stats = new Stats();
+document.body.appendChild(stats.dom);
 
-  renderer.render(scene, camera);
+// const clock = new THREE.Clock();
+// let delta; // using delta to make animations monitor frame-rate independent
+
+function animate() {
+  requestAnimationFrame(animate); // queues the function for another call
+
+  //   delta = clock.getDelta();
+
+  //   cube.rotation.x += delta;
+  //   cube.rotation.y += delta;
+
+  //   renderer.render(scene, camera); // changes made being re-rendered
+
+  stats.update();
 }
 
 animate();
+
+renderer.render(scene, camera);
