9659e0864f80eda7d76107e4678cbced41589414
Jatin Sharma <jatts0010@gmail.com>
2026-02-14 02:18:41 +0530

    object3d hierarchy

--- src/main.ts
+++ src/main.ts
@@ -349,6 +349,81 @@
 
 /* OBJECT3D */
 
+// import './style.css';
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import Stats from 'three/addons/libs/stats.module.js';
+// import { GUI } from 'dat.gui';
+
+// const scene = new THREE.Scene();
+// scene.add(new THREE.AxesHelper(5)); // axis lines
+
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000,
+// );
+// camera.position.set(1, 2, 3);
+
+// const renderer = new THREE.WebGLRenderer({ antialias: true });
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+// window.addEventListener('resize', () => {
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+// });
+
+// new OrbitControls(camera, renderer.domElement);
+
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshNormalMaterial({ wireframe: true });
+
+// const cube = new THREE.Mesh(geometry, material);
+// // cube.scale.set(2, 2, 2);
+// scene.add(cube);
+
+// const stats = new Stats();
+// document.body.appendChild(stats.dom);
+
+// const gui = new GUI();
+
+// const cubeFolder = gui.addFolder('Cube');
+// cubeFolder.add(cube, 'visible');
+// cubeFolder.open();
+
+// const positionFolder = cubeFolder.addFolder('Position');
+// positionFolder.add(cube.position, 'x', -5, 5);
+// positionFolder.add(cube.position, 'y', -5, 5);
+// positionFolder.add(cube.position, 'z', -5, 5);
+// positionFolder.open();
+
+// const rotationFolder = cubeFolder.addFolder('Rotation');
+// rotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2);
+// rotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2);
+// rotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2);
+// rotationFolder.open();
+
+// const scaleFolder = cubeFolder.addFolder('Scale');
+// scaleFolder.add(cube.scale, 'x', -5, 5);
+// scaleFolder.add(cube.scale, 'y', -5, 5);
+// scaleFolder.add(cube.scale, 'z', -5, 5);
+// scaleFolder.open();
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   renderer.render(scene, camera);
+
+//   stats.update();
+// }
+
+// animate();
+
+/* OBJECT3D HIERARCHY */
+
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
@@ -356,7 +431,7 @@ import Stats from 'three/addons/libs/stats.module.js';
 import { GUI } from 'dat.gui';
 
 const scene = new THREE.Scene();
-scene.add(new THREE.AxesHelper(5)); // axis lines
+scene.add(new THREE.AxesHelper(5));
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -364,11 +439,43 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000,
 );
-camera.position.set(1, 2, 3);
+camera.position.set(4, 4, 4);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(8, 0, 0);
+controls.update();
+
+const light = new THREE.PointLight(0xffffff, 400);
+light.position.set(10, 10, 10);
+scene.add(light);
+
+const object1 = new THREE.Mesh(
+  new THREE.SphereGeometry(),
+  new THREE.MeshPhongMaterial({ color: 0xff0000 }),
+);
+object1.position.set(4, 0, 0);
+scene.add(object1);
+object1.add(new THREE.AxesHelper(5));
+
+const object2 = new THREE.Mesh(
+  new THREE.SphereGeometry(),
+  new THREE.MeshPhongMaterial({ color: 0x00ff00 }),
+);
+object2.position.set(4, 0, 0);
+object1.add(object2);
+object2.add(new THREE.AxesHelper(5));
+
+const object3 = new THREE.Mesh(
+  new THREE.SphereGeometry(),
+  new THREE.MeshPhongMaterial({ color: 0x0000ff }),
+);
+object3.position.set(4, 0, 0);
+object2.add(object3);
+object3.add(new THREE.AxesHelper(5));
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -376,46 +483,71 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-new OrbitControls(camera, renderer.domElement);
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshNormalMaterial({ wireframe: true });
-
-const cube = new THREE.Mesh(geometry, material);
-// cube.scale.set(2, 2, 2);
-scene.add(cube);
+const gui = new GUI();
+const object1Folder = gui.addFolder('Object1 (Red Ball)');
+object1Folder.add(object1.position, 'x', 0, 10, 0.01).name('X Position');
+object1Folder
+  .add(object1.rotation, 'x', 0, Math.PI * 2, 0.01)
+  .name('X Rotation');
+object1Folder.add(object1.scale, 'x', 0, 2, 0.01).name('X Scale');
+object1Folder.add(object1, 'visible', 0, 2, 0.01).name('Visible');
+object1Folder.open();
+const object2Folder = gui.addFolder('Object2 (Green Ball)');
+object2Folder.add(object2.position, 'x', 0, 10, 0.01).name('X Position');
+object2Folder
+  .add(object2.rotation, 'x', 0, Math.PI * 2, 0.01)
+  .name('X Rotation');
+object2Folder.add(object2.scale, 'x', 0, 2, 0.01).name('X Scale');
+object2Folder.add(object2, 'visible', 0, 2, 0.01).name('Visible');
+object2Folder.open();
+const object3Folder = gui.addFolder('Object3 (Blue Ball)');
+object3Folder.add(object3.position, 'x', 0, 10, 0.01).name('X Position');
+object3Folder
+  .add(object3.rotation, 'x', 0, Math.PI * 2, 0.01)
+  .name('X Rotation');
+object3Folder.add(object3.scale, 'x', 0, 2, 0.01).name('X Scale');
+object3Folder.add(object3, 'visible', 0, 2, 0.01).name('Visible');
+object3Folder.open();
 
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
-const gui = new GUI();
-
-const cubeFolder = gui.addFolder('Cube');
-cubeFolder.add(cube, 'visible');
-cubeFolder.open();
-
-const positionFolder = cubeFolder.addFolder('Position');
-positionFolder.add(cube.position, 'x', -5, 5);
-positionFolder.add(cube.position, 'y', -5, 5);
-positionFolder.add(cube.position, 'z', -5, 5);
-positionFolder.open();
-
-const rotationFolder = cubeFolder.addFolder('Rotation');
-rotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2);
-rotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2);
-rotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2);
-rotationFolder.open();
-
-const scaleFolder = cubeFolder.addFolder('Scale');
-scaleFolder.add(cube.scale, 'x', -5, 5);
-scaleFolder.add(cube.scale, 'y', -5, 5);
-scaleFolder.add(cube.scale, 'z', -5, 5);
-scaleFolder.open();
+const debug = document.getElementById('debug') as HTMLDivElement;
 
 function animate() {
   requestAnimationFrame(animate);
 
   renderer.render(scene, camera);
+
+  const object1WorldPosition = new THREE.Vector3();
+  object1.getWorldPosition(object1WorldPosition);
+  const object2WorldPosition = new THREE.Vector3();
+  object2.getWorldPosition(object2WorldPosition);
+  const object3WorldPosition = new THREE.Vector3();
+  object3.getWorldPosition(object3WorldPosition);
+
+  debug.innerText =
+    'Red\n' +
+    'Local Pos X : ' +
+    object1.position.x.toFixed(2) +
+    '\n' +
+    'World Pos X : ' +
+    object1WorldPosition.x.toFixed(2) +
+    '\n' +
+    '\nGreen\n' +
+    'Local Pos X : ' +
+    object2.position.x.toFixed(2) +
+    '\n' +
+    'World Pos X : ' +
+    object2WorldPosition.x.toFixed(2) +
+    '\n' +
+    '\nBlue\n' +
+    'Local Pos X : ' +
+    object3.position.x.toFixed(2) +
+    '\n' +
+    'World Pos X : ' +
+    object3WorldPosition.x.toFixed(2) +
+    '\n';
 
   stats.update();
 }
