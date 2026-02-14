578ce666ced088247f0a65ad89d43831555aad21
Jatin Sharma <jatts0010@gmail.com>
2026-02-14 19:47:57 +0530

    added different materials

--- src/main.ts
+++ src/main.ts
@@ -556,6 +556,324 @@
 
 /* GEOMETRIES */
 
+// import './style.css';
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import Stats from 'three/addons/libs/stats.module.js';
+// import { GUI } from 'dat.gui';
+
+// const scene = new THREE.Scene();
+// scene.add(new THREE.AxesHelper(5));
+
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000,
+// );
+// camera.position.set(-2, 4, 5);
+
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+// new OrbitControls(camera, renderer.domElement);
+
+// const boxGeometry = new THREE.BoxGeometry(1, 2, 3, 4, 5, 6);
+// const sphereGeometry = new THREE.SphereGeometry();
+// const icosahedronGeometry = new THREE.IcosahedronGeometry();
+
+// console.log(boxGeometry);
+
+// const material = new THREE.MeshNormalMaterial({
+//   wireframe: true,
+// });
+
+// const cube = new THREE.Mesh(boxGeometry, material);
+// cube.position.x = -4;
+// scene.add(cube);
+
+// const sphere = new THREE.Mesh(sphereGeometry, material);
+// sphere.position.x = 0;
+// scene.add(sphere);
+
+// const icosahedron = new THREE.Mesh(icosahedronGeometry, material);
+// scene.add(icosahedron);
+// sphere.position.x = 4;
+
+// window.addEventListener('resize', () => {
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+// });
+
+// const stats = new Stats();
+// document.body.appendChild(stats.dom);
+
+// const gui = new GUI();
+
+// const cubeData = {
+//   width: 1,
+//   height: 1,
+//   depth: 1,
+//   widthSegments: 1,
+//   heightSegments: 1,
+//   depthSegments: 1,
+// };
+
+// const cubeFolder = gui.addFolder('Cube');
+// const cubeRotationFolder = cubeFolder.addFolder('Rotation');
+// cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2, 0.01);
+// cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2, 0.01);
+// cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2, 0.01);
+// const cubePositionFolder = cubeFolder.addFolder('Position');
+// cubePositionFolder.add(cube.position, 'x', -10, 10);
+// cubePositionFolder.add(cube.position, 'y', -10, 10);
+// cubePositionFolder.add(cube.position, 'z', -10, 10);
+// const cubeScaleFolder = cubeFolder.addFolder('Scale');
+// cubeScaleFolder.add(cube.scale, 'x', -5, 5, 0.1).onFinishChange(function () {
+//   console.log(cube.geometry);
+// });
+// cubeScaleFolder.add(cube.scale, 'y', -5, 5, 0.1);
+// cubeScaleFolder.add(cube.scale, 'z', -5, 5, 0.1);
+// cubeFolder
+//   .add(cubeData, 'width', 1, 30)
+//   .onChange(regenerateBoxGeometry)
+//   .onFinishChange(function () {
+//     console.log(cube.geometry);
+//   });
+// cubeFolder.add(cubeData, 'height', 1, 30).onChange(regenerateBoxGeometry);
+// cubeFolder.add(cubeData, 'depth', 1, 30).onChange(regenerateBoxGeometry);
+// cubeFolder
+//   .add(cubeData, 'widthSegments', 1, 30)
+//   .onChange(regenerateBoxGeometry);
+// cubeFolder
+//   .add(cubeData, 'heightSegments', 1, 30)
+//   .onChange(regenerateBoxGeometry);
+// cubeFolder
+//   .add(cubeData, 'depthSegments', 1, 30)
+//   .onChange(regenerateBoxGeometry);
+// cubeFolder.open();
+
+// function regenerateBoxGeometry() {
+//   const newGeometry = new THREE.BoxGeometry(
+//     cubeData.width,
+//     cubeData.height,
+//     cubeData.depth,
+//     cubeData.widthSegments,
+//     cubeData.heightSegments,
+//     cubeData.depthSegments,
+//   );
+//   cube.geometry.dispose();
+//   cube.geometry = newGeometry;
+// }
+
+// const sphereData = {
+//   radius: 1,
+//   widthSegments: 8,
+//   heightSegments: 6,
+//   phiStart: 0,
+//   phiLength: Math.PI * 2,
+//   thetaStart: 0,
+//   thetaLength: Math.PI,
+// };
+// const sphereFolder = gui.addFolder('Sphere');
+// sphereFolder
+//   .add(sphereData, 'radius', 0.1, 30)
+//   .onChange(regenerateSphereGeometry);
+// sphereFolder
+//   .add(sphereData, 'widthSegments', 1, 32)
+//   .onChange(regenerateSphereGeometry);
+// sphereFolder
+//   .add(sphereData, 'heightSegments', 1, 16)
+//   .onChange(regenerateSphereGeometry);
+// sphereFolder
+//   .add(sphereData, 'phiStart', 0, Math.PI * 2)
+//   .onChange(regenerateSphereGeometry);
+// sphereFolder
+//   .add(sphereData, 'phiLength', 0, Math.PI * 2)
+//   .onChange(regenerateSphereGeometry);
+// sphereFolder
+//   .add(sphereData, 'thetaStart', 0, Math.PI)
+//   .onChange(regenerateSphereGeometry);
+// sphereFolder
+//   .add(sphereData, 'thetaLength', 0, Math.PI)
+//   .onChange(regenerateSphereGeometry);
+// sphereFolder.open();
+
+// function regenerateSphereGeometry() {
+//   const newGeometry = new THREE.SphereGeometry(
+//     sphereData.radius,
+//     sphereData.widthSegments,
+//     sphereData.heightSegments,
+//     sphereData.phiStart,
+//     sphereData.phiLength,
+//     sphereData.thetaStart,
+//     sphereData.thetaLength,
+//   );
+//   sphere.geometry.dispose();
+//   sphere.geometry = newGeometry;
+// }
+
+// const icosahedronData = {
+//   radius: 1,
+//   detail: 0,
+// };
+// const icosahedronFolder = gui.addFolder('Icosahedron');
+// icosahedronFolder
+//   .add(icosahedronData, 'radius', 0.1, 10)
+//   .onChange(regenerateIcosahedronGeometry);
+// icosahedronFolder
+//   .add(icosahedronData, 'detail', 0, 5)
+//   .step(1)
+//   .onChange(regenerateIcosahedronGeometry);
+// icosahedronFolder.open();
+
+// function regenerateIcosahedronGeometry() {
+//   const newGeometry = new THREE.IcosahedronGeometry(
+//     icosahedronData.radius,
+//     icosahedronData.detail,
+//   );
+//   icosahedron.geometry.dispose();
+//   icosahedron.geometry = newGeometry;
+// }
+
+// const debug = document.getElementById('debug') as HTMLDivElement;
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   renderer.render(scene, camera);
+
+//   debug.innerText =
+//     'Matrix\n' + cube.matrix.elements.toString().replace(/,/g, '\n');
+
+//   stats.update();
+// }
+
+// animate();
+
+/* MATERIALS */
+
+// import './style.css';
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import Stats from 'three/addons/libs/stats.module.js';
+// import { GUI } from 'dat.gui';
+
+// const scene = new THREE.Scene();
+// scene.environment = new THREE.CubeTextureLoader()
+//   .setPath('https://sbcode.net/img/')
+//   .load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png']);
+
+// const axesHelper = new THREE.AxesHelper(5);
+// scene.add(axesHelper);
+
+// const gridHelper = new THREE.GridHelper();
+// gridHelper.position.y = -1;
+// scene.add(gridHelper);
+
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000,
+// );
+// camera.position.set(0, 2, 7);
+
+// const renderer = new THREE.WebGLRenderer({ antialias: true });
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
+
+// const boxGeometry = new THREE.BoxGeometry();
+
+// const sphereGeometry = new THREE.SphereGeometry();
+
+// const icosahedronGeometry = new THREE.IcosahedronGeometry();
+
+// const planeGeometry = new THREE.PlaneGeometry();
+
+// const torusKnotGeometry = new THREE.TorusKnotGeometry();
+
+// const material = new THREE.MeshStandardMaterial();
+// // const material = new THREE.MeshBasicMaterial();
+// // const material = new THREE.MeshNormalMaterial();
+
+// const cube = new THREE.Mesh(boxGeometry, material);
+// cube.position.set(5, 0, 0);
+// scene.add(cube);
+
+// const sphere = new THREE.Mesh(sphereGeometry, material);
+// sphere.position.set(3, 0, 0);
+// scene.add(sphere);
+
+// const icosahedron = new THREE.Mesh(icosahedronGeometry, material);
+// icosahedron.position.set(0, 0, 0);
+// scene.add(icosahedron);
+
+// const plane = new THREE.Mesh(planeGeometry, material);
+// plane.position.set(-2, 0, 0);
+// scene.add(plane);
+
+// const torusKnot = new THREE.Mesh(torusKnotGeometry, material);
+// torusKnot.position.set(-5, 0, 0);
+// scene.add(torusKnot);
+
+// window.addEventListener('resize', () => {
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+// });
+
+// const stats = new Stats();
+// document.body.appendChild(stats.dom);
+
+// const options = {
+//   side: {
+//     FrontSide: THREE.FrontSide,
+//     BackSide: THREE.BackSide,
+//     DoubleSide: THREE.DoubleSide,
+//   },
+// };
+
+// const gui = new GUI();
+
+// const materialFolder = gui.addFolder('THREE.Material');
+// materialFolder
+//   .add(material, 'transparent')
+//   .onChange(() => (material.needsUpdate = true));
+// materialFolder.add(material, 'opacity', 0, 1, 0.01);
+// materialFolder
+//   .add(material, 'alphaTest', 0, 1, 0.01)
+//   .onChange(() => updateMaterial());
+// materialFolder.add(material, 'visible');
+// materialFolder
+//   .add(material, 'side', options.side)
+//   .onChange(() => updateMaterial());
+// materialFolder.open();
+
+// function updateMaterial() {
+//   material.side = Number(material.side) as THREE.Side;
+//   material.needsUpdate = true; // for when material recompilation is required
+// }
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   controls.update();
+
+//   renderer.render(scene, camera);
+
+//   stats.update();
+// }
+
+// animate();
+
+/* COMMON MATERIALS */
+
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
@@ -563,43 +881,24 @@ import Stats from 'three/addons/libs/stats.module.js';
 import { GUI } from 'dat.gui';
 
 const scene = new THREE.Scene();
-scene.add(new THREE.AxesHelper(5));
+// scene.environment = new THREE.CubeTextureLoader()
+//   .setPath('https://sbcode.net/img/')
+//   .load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png']);
+
+const gridHelper = new THREE.GridHelper();
+scene.add(gridHelper);
 
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000,
+  100,
 );
-camera.position.set(-2, 4, 5);
+camera.position.set(-1, 4, 2.5);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-new OrbitControls(camera, renderer.domElement);
-
-const boxGeometry = new THREE.BoxGeometry(1, 2, 3, 4, 5, 6);
-const sphereGeometry = new THREE.SphereGeometry();
-const icosahedronGeometry = new THREE.IcosahedronGeometry();
-
-console.log(boxGeometry);
-
-const material = new THREE.MeshNormalMaterial({
-  wireframe: true,
-});
-
-const cube = new THREE.Mesh(boxGeometry, material);
-cube.position.x = -4;
-scene.add(cube);
-
-const sphere = new THREE.Mesh(sphereGeometry, material);
-sphere.position.x = 0;
-scene.add(sphere);
-
-const icosahedron = new THREE.Mesh(icosahedronGeometry, material);
-scene.add(icosahedron);
-sphere.position.x = 4;
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -607,146 +906,125 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+const light = new THREE.DirectionalLight(undefined, Math.PI);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+const data = { color: 0x00ff00, labelsVisible: true };
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+);
+plane.rotation.x = -Math.PI / 2;
+plane.visible = false;
+scene.add(plane);
+
+const geometry = new THREE.IcosahedronGeometry(1, 1);
+
+const meshes = [
+  new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: data.color })),
+  new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({ flatShading: true })),
+  new THREE.Mesh(
+    geometry,
+    new THREE.MeshPhongMaterial({ color: data.color, flatShading: true }),
+  ),
+  new THREE.Mesh(
+    geometry,
+    new THREE.MeshStandardMaterial({ color: data.color, flatShading: true }),
+  ),
+];
+
+meshes[0].position.set(-3, 1, 0);
+meshes[1].position.set(-1, 1, 0);
+meshes[2].position.set(1, 1, 0);
+meshes[3].position.set(3, 1, 0);
+
+scene.add(...meshes);
+
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
 const gui = new GUI();
 
-const cubeData = {
-  width: 1,
-  height: 1,
-  depth: 1,
-  widthSegments: 1,
-  heightSegments: 1,
-  depthSegments: 1,
-};
-
-const cubeFolder = gui.addFolder('Cube');
-const cubeRotationFolder = cubeFolder.addFolder('Rotation');
-cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2, 0.01);
-cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2, 0.01);
-cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2, 0.01);
-const cubePositionFolder = cubeFolder.addFolder('Position');
-cubePositionFolder.add(cube.position, 'x', -10, 10);
-cubePositionFolder.add(cube.position, 'y', -10, 10);
-cubePositionFolder.add(cube.position, 'z', -10, 10);
-const cubeScaleFolder = cubeFolder.addFolder('Scale');
-cubeScaleFolder.add(cube.scale, 'x', -5, 5, 0.1).onFinishChange(function () {
-  console.log(cube.geometry);
+const meshBasicMaterialFolder = gui.addFolder('MeshBasicMaterial');
+meshBasicMaterialFolder.addColor(data, 'color').onChange(() => {
+  (meshes[0].material as THREE.MeshBasicMaterial).color.set(data.color);
 });
-cubeScaleFolder.add(cube.scale, 'y', -5, 5, 0.1);
-cubeScaleFolder.add(cube.scale, 'z', -5, 5, 0.1);
-cubeFolder
-  .add(cubeData, 'width', 1, 30)
-  .onChange(regenerateBoxGeometry)
-  .onFinishChange(function () {
-    console.log(cube.geometry);
+meshBasicMaterialFolder.add(meshes[0].material, 'wireframe');
+meshBasicMaterialFolder.open();
+
+const meshNormalMaterialFolder = gui.addFolder('MeshNormalMaterial');
+meshNormalMaterialFolder
+  .add(meshes[1].material as THREE.MeshNormalMaterial, 'flatShading')
+  .onChange(() => {
+    meshes[1].material.needsUpdate = true;
   });
-cubeFolder.add(cubeData, 'height', 1, 30).onChange(regenerateBoxGeometry);
-cubeFolder.add(cubeData, 'depth', 1, 30).onChange(regenerateBoxGeometry);
-cubeFolder
-  .add(cubeData, 'widthSegments', 1, 30)
-  .onChange(regenerateBoxGeometry);
-cubeFolder
-  .add(cubeData, 'heightSegments', 1, 30)
-  .onChange(regenerateBoxGeometry);
-cubeFolder
-  .add(cubeData, 'depthSegments', 1, 30)
-  .onChange(regenerateBoxGeometry);
-cubeFolder.open();
+meshNormalMaterialFolder.add(meshes[1].material, 'wireframe');
+meshNormalMaterialFolder.open();
 
-function regenerateBoxGeometry() {
-  const newGeometry = new THREE.BoxGeometry(
-    cubeData.width,
-    cubeData.height,
-    cubeData.depth,
-    cubeData.widthSegments,
-    cubeData.heightSegments,
-    cubeData.depthSegments,
-  );
-  cube.geometry.dispose();
-  cube.geometry = newGeometry;
-}
+const meshPhongMaterialFolder = gui.addFolder('MeshPhongMaterial');
+meshPhongMaterialFolder.addColor(data, 'color').onChange(() => {
+  (meshes[2].material as THREE.MeshPhongMaterial).color.set(data.color);
+});
+meshPhongMaterialFolder
+  .add(meshes[2].material as THREE.MeshPhongMaterial, 'flatShading')
+  .onChange(() => {
+    meshes[2].material.needsUpdate = true;
+  });
+meshPhongMaterialFolder.add(meshes[2].material, 'wireframe');
+meshPhongMaterialFolder.open();
 
-const sphereData = {
-  radius: 1,
-  widthSegments: 8,
-  heightSegments: 6,
-  phiStart: 0,
-  phiLength: Math.PI * 2,
-  thetaStart: 0,
-  thetaLength: Math.PI,
-};
-const sphereFolder = gui.addFolder('Sphere');
-sphereFolder
-  .add(sphereData, 'radius', 0.1, 30)
-  .onChange(regenerateSphereGeometry);
-sphereFolder
-  .add(sphereData, 'widthSegments', 1, 32)
-  .onChange(regenerateSphereGeometry);
-sphereFolder
-  .add(sphereData, 'heightSegments', 1, 16)
-  .onChange(regenerateSphereGeometry);
-sphereFolder
-  .add(sphereData, 'phiStart', 0, Math.PI * 2)
-  .onChange(regenerateSphereGeometry);
-sphereFolder
-  .add(sphereData, 'phiLength', 0, Math.PI * 2)
-  .onChange(regenerateSphereGeometry);
-sphereFolder
-  .add(sphereData, 'thetaStart', 0, Math.PI)
-  .onChange(regenerateSphereGeometry);
-sphereFolder
-  .add(sphereData, 'thetaLength', 0, Math.PI)
-  .onChange(regenerateSphereGeometry);
-sphereFolder.open();
+const meshStandardMaterialFolder = gui.addFolder('MeshStandardMaterial');
+meshStandardMaterialFolder.addColor(data, 'color').onChange(() => {
+  (meshes[3].material as THREE.MeshStandardMaterial).color.set(data.color);
+});
+meshStandardMaterialFolder
+  .add(meshes[3].material as THREE.MeshStandardMaterial, 'flatShading')
+  .onChange(() => {
+    meshes[3].material.needsUpdate = true;
+  });
+meshStandardMaterialFolder.add(meshes[3].material, 'wireframe');
+meshStandardMaterialFolder.open();
 
-function regenerateSphereGeometry() {
-  const newGeometry = new THREE.SphereGeometry(
-    sphereData.radius,
-    sphereData.widthSegments,
-    sphereData.heightSegments,
-    sphereData.phiStart,
-    sphereData.phiLength,
-    sphereData.thetaStart,
-    sphereData.thetaLength,
-  );
-  sphere.geometry.dispose();
-  sphere.geometry = newGeometry;
-}
+const lightFolder = gui.addFolder('Light');
+lightFolder.add(light, 'visible');
+lightFolder.open();
 
-const icosahedronData = {
-  radius: 1,
-  detail: 0,
-};
-const icosahedronFolder = gui.addFolder('Icosahedron');
-icosahedronFolder
-  .add(icosahedronData, 'radius', 0.1, 10)
-  .onChange(regenerateIcosahedronGeometry);
-icosahedronFolder
-  .add(icosahedronData, 'detail', 0, 5)
-  .step(1)
-  .onChange(regenerateIcosahedronGeometry);
-icosahedronFolder.open();
+const gridFolder = gui.addFolder('Grid');
+gridFolder.add(gridHelper, 'visible');
+gridFolder.open();
 
-function regenerateIcosahedronGeometry() {
-  const newGeometry = new THREE.IcosahedronGeometry(
-    icosahedronData.radius,
-    icosahedronData.detail,
-  );
-  icosahedron.geometry.dispose();
-  icosahedron.geometry = newGeometry;
-}
+const labelsFolder = gui.addFolder('Labels');
+labelsFolder.add(data, 'labelsVisible');
+labelsFolder.open();
 
-const debug = document.getElementById('debug') as HTMLDivElement;
+const labels = document.querySelectorAll<HTMLDivElement>('.label');
+
+let x, y;
+const v = new THREE.Vector3();
 
 function animate() {
   requestAnimationFrame(animate);
 
-  renderer.render(scene, camera);
+  controls.update();
 
-  debug.innerText =
-    'Matrix\n' + cube.matrix.elements.toString().replace(/,/g, '\n');
+  for (let i = 0; i < 4; i++) {
+    v.copy(meshes[i].position);
+    v.project(camera);
+
+    x = ((1 + v.x) / 2) * innerWidth - 50;
+    y = ((1 - v.y) / 2) * innerHeight;
+
+    labels[i].style.left = x + 'px';
+    labels[i].style.top = y + 'px';
+    labels[i].style.display = data.labelsVisible ? 'block' : 'none';
+  }
+
+  renderer.render(scene, camera);
 
   stats.update();
 }
